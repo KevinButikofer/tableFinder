2fe9aa7ce442f91c12692e936fb886777ccbea29
Kevin Butikofer <kevin.butikofer@outlook.com>
2020-04-02 22:17:12 +0200

    add restaurants page not working

---
+++ table_finder/src/components/Restaurants.vue
@@ -0,0 +1,28 @@
+  <template>
+  <h2>{{msg}}</h2>
+  </template>
+
+<script>
+export default {
+  name: 'Restaurants',
+    props: {
+    msg: String
+  }
+}
+</script>
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style
---
+++ table_finder/src/views/RestaurantsView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="restaurantsView">
+    <h1>Restaurants View</h1>
+    <Restaurants msg="toto"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Restaurants from '@/components/Restaurants.vue'
+
+export default {
+  name: 'RestaurantsView',
+  components: {
+    Restaurants
+  }
+}
+</script>
