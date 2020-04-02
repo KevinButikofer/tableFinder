ec3d996f81c33f5dca37c46fdba300559c74b1bc
Kevin Butikofer <kevin.butikofer@outlook.com>
2020-04-02 21:09:22 +0200

    add router

---
+++ table_finder/src/components/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar color="primary">
+        <ion-title>Home</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content padding>
+      <ion-list>
+        <ion-item>
+          <ion-checkbox slot="start"></ion-checkbox>
+          <ion-label>
+            <h1>Create Idea</h1>
+            <ion-note>Run Idea by Brandy</ion-note>
+          </ion-label>
+          <ion-badge color="success" slot="end">5 Days</ion-badge>
+        </ion-item>
+      </ion-list>
+      <ion-fab vertical="bottom" horizontal="end" slot="fixed">
+        <ion-fab-button @click="$router.push({ name: 'new-item' })">
+          <ion-icon name="add" />
+        </ion-fab-button>
+      </ion-fab>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import { add } from "ionicons/icons";
+import { addIcons } from "ionicons";
+addIcons({
+  "ios-add": add.ios,
+  "md-add": add.md
+});
+export default {
+  name: "HomePage",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- table_finder/src/main.js
+++ table_finder/src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router/index'
 
-Vue.config.productionTip = false
+
+import { IonicVueRouter } from "@ionic/vue";
+Vue.use(IonicVueRouter);
+
+Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
