91ed7a7a2a67cea5d427d8306d991952d327afdc
Lewis Jaggi <lewisjaggi@gmail.com>
2020-04-22 23:12:54 +0200

    reload slice down on viewrest

--- src/views/RestaurantView.vue
+++ src/views/RestaurantView.vue
@@ -2,6 +2,9 @@
     <ion-app>
         <Header title="Find a table"/>
         <ion-content>
+            <ion-refresher slot="fixed" @ionRefresh="doRefresh($event)">
+                <ion-refresher-content></ion-refresher-content>
+            </ion-refresher>
             <ion-grid>
 
                 <ClientInfo/>
@@ -10,7 +13,7 @@
             <ion-grid>
 
                 <booking-info/>
-                
+
             </ion-grid>
         </ion-content>
     </ion-app>
@@ -23,9 +26,19 @@
 
     export default {
         name: "RestaurantView",
+        methods: {
+            doRefresh(event) {
+                console.log('Begin async operation');
+
+                setTimeout(() => {
+                    console.log('Async operation has ended');
+                    event.target.complete();
+                }, 2000);
+            }
+        },
         components: {
             ClientInfo,
-            Header, 
+            Header,
             BookingInfo
         }
     }
