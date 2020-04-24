ee0b0de8310bf1bec2cdfb5bfd0dcf52d0022a5a
Lewis Jaggi <lewisjaggi@gmail.com>
2020-04-24 11:39:27 +0200

    remake tabs

--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -1,30 +1,25 @@
 <template>
     <ion-app>
         <ion-tabs>
-            <ion-tab tab="home" >
+            <ion-tab tab="home">
                 <Home/>
             </ion-tab>
 
-            <ion-tab tab="about">
-                <ion-vue-router name="about"></ion-vue-router>
+            <ion-tab tab="booking">
+                <ion-vue-router name="booking"></ion-vue-router>
             </ion-tab>
             <ion-tab tab="restaurant">
                 <RestaurantView/>
             </ion-tab>
 
             <ion-tab-bar slot="bottom">
-                <ion-tab-button tab="home">
-                    <ion-icon name="navigate"></ion-icon>
-                    <ion-label>Home</ion-label>
-                </ion-tab-button>
-
-                <ion-tab-button tab="about">
-                    <ion-icon name="person"></ion-icon>
-                    <ion-label>About</ion-label>
+                <ion-tab-button tab="booking">
+                    <ion-icon name="book"></ion-icon>
+                    <ion-label>Booking</ion-label>
                 </ion-tab-button>
                 <ion-tab-button tab="restaurant">
-                    <ion-icon name="bookmarks"></ion-icon>
-                    <ion-label>Restaurant</ion-label>
+                    <ion-icon name="restaurant"></ion-icon>
+                    <ion-label>Search</ion-label>
                 </ion-tab-button>
             </ion-tab-bar>
         </ion-tabs>
@@ -32,12 +27,11 @@
 </template>
 
 <script>
-    import Home from "./Home";
     import RestaurantView from "./RestaurantView";
+
     export default {
         name: "Tabs",
-        components:{
-            Home,
+        components: {
             RestaurantView
         }
     }
