5cdd74e831e890aa45222cfcb16c1515e5805f7b
Noah <graells.dev@gmail.com>
2020-04-24 11:18:59 +0200

    Creating Restaurant infos page

---
+++ src/components/LocationCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <ion-card>
+      <ion-card-header>
+        <ion-card-title>Location</ion-card-title>
+      </ion-card-header>
+      <ion-card-content>
+        TODO : Insert map
+      </ion-card-content>
+    </ion-card>
+</template>
+
+<script>
+  export default {
+      name: "BookingInfoCard",
+  }
+</script>
+
+<style scoped>
+ion-card {
+text-align:left;
+}
+</style>
