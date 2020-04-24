be7fe329f95d668edd66dd52297ccb0b2be3e291
Lewis Jaggi <lewisjaggi@gmail.com>
2020-04-24 15:43:26 +0200

    vue route

--- src/components/ResultsList.vue
+++ src/components/ResultsList.vue
@@ -1,26 +1,31 @@
 <template>
     <ion-list>
-    
-        <ion-item
-                v-for="(item, index) in filteredRestaurant"
-                :key="index"
-                :value="item" @click="showRestaurant(item)">
-            <ion-thumbnail slot="start">
-                <img :src="item.image">
-            </ion-thumbnail>
-            <ion-badge v-if="item.place > 0" color="success" slot="end">{{item.place}}</ion-badge>
-            <ion-badge v-else color="danger" slot="end">{{item.place}}</ion-badge>
-            <ion-label>
-                <h2>{{item.name}}</h2>
-                <h3>{{item.address}}, {{item.city}}</h3>
-                <p>
-                    <ion-icon v-for="(n,index) in item.rating" :key="index" name="star">{{n}}</ion-icon>
-                    <ion-icon v-for="(x,index1) in (MAXNOTE-item.rating)" :key="index1+MAXNOTE" name="star-outline">{{x}}
-                    </ion-icon>
+        <ion-item-sliding v-for="(item, index) in filteredRestaurant"
+                          :key="index"
+                          :value="item">
+            <ion-item-options side="end">
+                <ion-item-option  color="success">Book</ion-item-option>
+            </ion-item-options>
 
-                </p>
-            </ion-label>
-        </ion-item>
+            <ion-item @click="showRestaurant(item)">
+                <ion-thumbnail slot="start">
+                    <img :src="item.image">
+                </ion-thumbnail>
+                <ion-badge v-if="item.place > 0" color="success" slot="end">{{item.place}}</ion-badge>
+                <ion-badge v-else color="danger" slot="end">{{item.place}}</ion-badge>
+                <ion-label>
+                    <h2>{{item.name}}</h2>
+                    <h3>{{item.address}}, {{item.city}}</h3>
+                    <p>
+                        <ion-icon v-for="(n,index) in item.rating" :key="index" name="star">{{n}}</ion-icon>
+                        <ion-icon v-for="(x,index1) in (MAXNOTE-item.rating)" :key="index1+MAXNOTE" name="star-outline">
+                            {{x}}
+                        </ion-icon>
+
+                    </p>
+                </ion-label>
+            </ion-item>
+        </ion-item-sliding>
     </ion-list>
 
 </template>
@@ -28,7 +33,7 @@
 <script>
     import {RESTAURANTS} from "../datas/restaurants";
     import {MAXNOTE} from "../datas/notes"
-    
+
 
     export default {
         name: "ResultList",
@@ -38,40 +43,38 @@
                 MAXNOTE: MAXNOTE
             }
         },
-        computed: {            
-            filteredRestaurant :
-            {
-                get(){
-                    if(this.$store.getters.localisation == "")
-                    {
-                        return RESTAURANTS
-                    }
-                    var filteredRestaurant = [];
-                   RESTAURANTS.forEach((rest) => {
-                            // if(rest.place > this.$store.state.peopleNumber 
-                            // && rest.location == this.$store.state.location
-                            //  && rest.foodStyle == this.$store.state.foodStyle)
-                            // console.log(rest.city);
-                            // console.log(this.$store.getters.localisation);
-                            if(rest.city == this.$store.getters.localisation) // && ((typeof(this.$store.state.foodStyle) != "undefined" & rest.foodStyle in this.$store.state.foodStyle) || this.$store.state.foodStyle.length < 1))
-                            {
-                                filteredRestaurant.push(rest);
+        computed: {
+            filteredRestaurant:
+                {
+                    get() {
+                        if (this.$store.getters.localisation == "") {
+                            return RESTAURANTS
+                        }
+                        var filteredRestaurant = [];
+                        RESTAURANTS.forEach((rest) => {
+                                // if(rest.place > this.$store.state.peopleNumber
+                                // && rest.location == this.$store.state.location
+                                //  && rest.foodStyle == this.$store.state.foodStyle)
+                                // console.log(rest.city);
+                                // console.log(this.$store.getters.localisation);
+                                if (rest.city == this.$store.getters.localisation) // && ((typeof(this.$store.state.foodStyle) != "undefined" & rest.foodStyle in this.$store.state.foodStyle) || this.$store.state.foodStyle.length < 1))
+                                {
+                                    filteredRestaurant.push(rest);
+                                }
                             }
-                       }
-                   );                   
-                    return filteredRestaurant;
+                        );
+                        return filteredRestaurant;
+                    }
                 }
-            }
         },
         methods:
-        {
-            showRestaurant:function(rest)
             {
-                this.$store.dispatch("fetchSelectedRestaurant", rest);
-                this.$router.push({name: 'RestaurantInfo'})
-                console.log(this.$store.getters.selectedRestaurant);
+                showRestaurant: function (rest) {
+                    this.$store.dispatch("fetchSelectedRestaurant", rest);
+                    this.$router.push({name: 'RestaurantInfo'})
+                    console.log(this.$store.getters.selectedRestaurant);
+                }
             }
-        }
     }
 </script>
 
